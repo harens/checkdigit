bdcb7dd2884b08cd1648e8c4276f4de47375a9c3
Haren <harensdeveloper@gmail.com>
2019-01-20 21:51:37 +0000

    Travis Pass Changed

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md") as f:
 
 setup(
     name="checkdigit",
-    version="0.0.2",
+    version="0.0.3",
     description="Checking digits with a digit!",
     long_description=long_description,
     long_description_content_type="text/markdown",
