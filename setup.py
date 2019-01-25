a6b6d2d573e4786dbeefdc5def070910967bf16e
Haren <harensdeveloper@gmail.com>
2019-01-25 13:46:48 +0000

    Prep for 0.0.6 Release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md") as f:
 
 setup(
     name="checkdigit",
-    version="0.0.5",
+    version="0.0.6",
     description="Checking digits with a digit!",
     long_description=long_description,
     long_description_content_type="text/markdown",
